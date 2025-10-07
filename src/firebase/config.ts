fa894ab84cc55ef20210f586fdb32c49d9a4469c
Abubakar Mi <abubakarmi131@gmail.com>
2025-10-07 15:36:34 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be populated by the backend.
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
+  apiKey: "AIzaSyA7fgreL9wbMaSv3lPxmt5nzC38Vho_OcQ",
   authDomain: "rewardify-bae42.firebaseapp.com",
   projectId: "rewardify-bae42",
-  storageBucket: "rewardify-bae42.appspot.com",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  storageBucket: "rewardify-bae42.firebasestorage.app",
+  messagingSenderId: "1063169223827",
+  appId: "1:1063169223827:web:470742296b3f0a4c8a3166",
+  measurementId: "G-029RDET8YS"
 };
